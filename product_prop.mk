1366a1c06319fe86b3513904f2dd6adfcd221578
Kshitij Gupta <kshitijgm@gmail.com>
2021-06-05 02:44:55 +0800

    juice: Enable running without sync framework
    Signed-off-by: aseelps <aseelpsathar@gmail.com>

--- product_prop.mk
+++ product_prop.mk
@@ -12,6 +12,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PRODUCT_PROPERTIES += \
     debug.hwui.renderer=skiavk \
     ro.surface_flinger.max_frame_buffer_acquired_buffers=3 \
+    ro.surface_flinger.running_without_sync_framework=true \
     debug.sf.latch_unsignaled=1 \
     debug.sf.disable_backpressure=1
 
