6bf1891860a144b7961ad89bb87cd9c58d49c05b
aseelps <aseelpsathar@gmail.com>
2021-07-01 19:25:26 +0530

    juice: Switch to Blur prop
    Signed-off-by: aseelps <aseelpsathar@gmail.com>

--- product_prop.mk
+++ product_prop.mk
@@ -2,6 +2,12 @@
 PRODUCT_PROPERTY_OVERRIDES += \
     debug.sf.disable_hwc_overlays=1
 
+#Blur
+PRODUCT_PRODUCT_PROPERTIES += \
+ro.sf.blurs_are_expensive=1 \
+ro.surface_flinger.supports_background_blur=1 \
+debug.sf.disable_backpressure=1
+
 # Dex2oat
 PRODUCT_PROPERTY_OVERRIDES += \
     dalvik.vm.dex2oat64.enabled=true
